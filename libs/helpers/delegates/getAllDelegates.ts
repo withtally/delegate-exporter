d08ebf242be1bd893d48d42e8ff56d97d66b444b
Nazar-Brunarskyi <3nnazar836@gmail.com>
2024-03-26 16:46:01 +0200

    add limits

--- libs/helpers/delegates/getAllDelegates.ts
+++ libs/helpers/delegates/getAllDelegates.ts
@@ -11,7 +11,7 @@ export const getAllDelegates = async (afterCursor?: string) => {
       delegates.push(...nodes);
 
       afterCursor = lastCursor;
-    } while (afterCursor && afterCursor.length > 0 && delegates.length < 2000);
+    } while (afterCursor && afterCursor.length > 0 && delegates.length < 1000);
 
     return delegates;
   } catch (error) {
