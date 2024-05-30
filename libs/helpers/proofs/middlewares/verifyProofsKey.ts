4304c9352d679575c2c721b0fd7e08da18207504
Nazar-Brunarskyi <3nnazar836@gmail.com>
2024-05-30 15:01:34 +0300

    add additional protection for the proof endpoint

--- libs/helpers/proofs/middlewares/verifyProofsKey.ts
+++ libs/helpers/proofs/middlewares/verifyProofsKey.ts
@@ -1,18 +1,26 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export const verifyProofsKey = (
-    req: VercelRequest,
-    res: VercelResponse,
-    next: Function
+  req: VercelRequest,
+  res: VercelResponse,
+  next: Function
 ) => {
-    if (
-        req.headers["proofs_load_key"] !== process.env.PROOFS_LOAD_KEY
-    ) {
-        res
-            .setHeader("Content-Type", "application/json")
-            .status(403)
-            .json({ message: "Access Denied" });
-        return;
-    }
-    next();
+  if (!req.headers["proofs_load_key"]) {
+    res
+      .setHeader("Content-Type", "application/json")
+      .status(400)
+      .json({ message: "Header 'proofs_load_key' is missing" });
+
+    return;
+  }
+
+  if (req.headers["proofs_load_key"] !== process.env.PROOFS_LOAD_KEY) {
+    res
+      .setHeader("Content-Type", "application/json")
+      .status(403)
+      .json({ message: "Access Denied" });
+
+    return;
+  }
+  next();
 };
